20af2acc691babeb5865d273f0163848941368f7
guptasonu14 <sguptabudha@gmail.com>
2022-10-15 23:19:44 +0530

    add event listner

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 'use strict';
-
+/*
 console.log(document.querySelector('.message').
 textContent);
-
 document.querySelector('.message').textContent = '🎉 Correct Number!';
 
 document.querySelector('.number').textContent =13;
@@ -10,4 +9,25 @@ document.querySelector('.score').textContent= 10;
 
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
+*/
+const x = function(){
+    console.log(23);
+}
+
+document.querySelector('.check').addEventListener('click', function () {
+const guess = Number(document.querySelector('.guess').value);
+console.log(guess, typeof guess);
+
+
+  // When there is no input
+  if (!guess) {
+    // document.querySelector('.message').textContent = '⛔️ No number!';
+    displayMessage('⛔️ No number!');
+
+    // When player wins
+  }
+});
+
+
+
 
